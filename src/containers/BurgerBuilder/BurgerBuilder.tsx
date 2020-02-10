573ebfe8052a931655d1f4caa8889d0a1d841af6
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-10 12:13:02 +0200

    Setup project and start using CSS modules

---
+++ src/containers/BurgerBuilder/BurgerBuilder.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Auxiliary from './../../hoc/Auxiliary';
+
+const BurgerBuilder = () => {
+    return (
+        <Auxiliary>
+            <div>Burger</div>
+            <div>Build Controls</div>
+        </Auxiliary>
+    )
+}
+
+export default BurgerBuilder
