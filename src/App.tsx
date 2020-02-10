cab2a1aa8cf4eecbe0960169f4332754994a2e01
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-02-10 13:36:40 +0200

    Boilerplate and Typescript Plugin for CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import React from "react";
 
 const App = () => {
   return (
@@ -10,6 +10,6 @@ const App = () => {
       </Layout>
     </div>
   );
-}
+};
 
 export default App;
